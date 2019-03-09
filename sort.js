3d4871bdf6af86478673c1ebe6ed760b1774af3a
Nick Lindeberg <lindebergnick@gmail.com>
2019-03-09 11:30:00 -0700

    bubble sort working and well tested

--- sort.js
+++ sort.js
@@ -1,19 +1,20 @@
 pry = require('pryjs')
 
-function flip(array){
-  sorted_array = array.reverse();
-  return sorted_array
-};
-
-function findHigh(ary){
-  high_num = Math.max(...ary);
-  return high_num
-};
-
-
 function sort(array) {
   length = array.length
-
+  var swapped;
+  do {
+    swapped = false;
+    for (var element = 1; element < length; ++element){
+      if (array[element - 1] > array[element]) {
+        [array[element], array[element-1]] = [array[element-1], array[element]];
+        swapped = true;
+      }
+    }
+  } while (swapped == true)
+  return array
 }
 
-console.log(sort([1,2]))
+console.log(sort([2,1,3,5,6,7,8,0,1,4,6,7]))
+
+module.exports = sort
--- test/sort_test.js
+++ test/sort_test.js
@@ -1,11 +1,31 @@
 const expect = require('chai').expect
-const flip = require('../sort')
+const sort = require('../sort')
 
-describe("sort functionality", function() {
-  context("sort function", function(){
-    it('can sort an array', function(){
-      var array = flip([2,1])
+pry = require('pryjs')
+
+describe("Sort", function() {
+  context("array of two numbers", function() {
+    it("will sort an array of two numbers", function() {
+      var array = sort([2,1]);
       expect(array).to.deep.equal([1,2])
+    })
+  })
+  context("array of three numbers", function() {
+    it("will sort an array of three numbers", function() {
+      var array = sort([3,2,1]);
+      expect(array).to.deep.equal([1,2,3])
+    })
+  })
+  context("array of three numbers", function() {
+    it("will sort an array of three numbers again", function() {
+      var array = sort([2,3,1]);
+      expect(array).to.deep.equal([1,2,3])
+    })
+  })
+  context("array of two numbers", function() {
+    it("will sort an array of two numbers", function() {
+      var array = sort([3,2,1,4,5,9,8,7,6]);
+      expect(array).to.deep.equal([1,2,3,4,5,6,7,8,9])
     })
   })
 })
